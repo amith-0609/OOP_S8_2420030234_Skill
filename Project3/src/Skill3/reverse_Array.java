4a38c49d83c1f7e3ba2b4c31eea0b43d185d7856
amith-0609 <yakkalaamith@gmail.com>
2025-08-06 11:28:13 +0530

    Add files via upload

---
+++ Project3/src/Skill3/reverse_Array.java
@@ -0,0 +1,25 @@
+package Skill3;
+import java.util.*;
+
+public class reverse_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter size: ");
+	        int n = sc.nextInt();
+	        int[] arr = new int[n];
+
+	        System.out.println("Enter array:");
+	        
+	        for (int i = 0; i < n; i++)
+	            arr[i] = sc.nextInt();
+
+	        System.out.println("Reversed array:");
+	        
+	        for (int i = n - 1; i >= 0; i--)
+	            System.out.print(arr[i] + " ");
+
+	}
+
+}
---
+++ Project3/src/Skill3/user_Name_Formatter.java
@@ -0,0 +1,18 @@
+package Skill3;
+import java.util.*;
+
+public class user_Name_Formatter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		System.out.println("Enter :");
+		Scanner sc =new Scanner(System.in);
+        String name = sc.next();
+        name.trim();
+        name.toLowerCase();
+        name.replace("a", "@");
+        System.out.println("Formatted Name: " + name);
+
+	}
+
+}
